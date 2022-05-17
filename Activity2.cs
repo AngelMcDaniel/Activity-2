c766a9508b31a43bdff4c2d840a150bb9fcea9d8
AngelMcDaniel <105662216+AngelMcDaniel@users.noreply.github.com>
2022-05-16 22:31:31 -0500

    Add files via upload

---
+++ Activity2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_2
+{
+    public partial class Activity2 : Form
+    {
+        double numConverter = 0;
+
+        public Activity2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            outputBox2.Text = Convert.ToString(numConverter * 3);
+
+            string message = outputBox2.Text;
+            string title = "Message Box";
+            MessageBox.Show(message, title);
+        }
+
+        private void inputBox1_TextChanged(object sender, EventArgs e)
+        {
+            numConverter = Convert.ToDouble(inputBox1.Text);
+        }
+
+        private void outputBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
